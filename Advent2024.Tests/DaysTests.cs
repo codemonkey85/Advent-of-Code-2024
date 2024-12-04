0e4ce6a72ed7fef45bf22a0889ea103b54390bc3
Michael Bond <michael@bondcodes.com>
2024-12-04 11:24:18 -0500

    Slightly better approach to unit test gross hack

--- Advent2024.Tests/DaysTests.cs
+++ Advent2024.Tests/DaysTests.cs
@@ -6,22 +6,20 @@ namespace Advent2024.Tests;
 
 public static class Tests
 {
-    private const string InputFieldName = "input";
-    private const string TestInputsDir = "TestInputs";
-    private const string InputsDir = "Inputs";
-
     [TestCase(typeof(Day01), "11", "31")]
     [TestCase(typeof(Day02), "2", "4")]
     [TestCase(typeof(Day03), "161", "")] // TODO: Need to account for different test data in part 2
     [TestCase(typeof(Day04), "", "")]
     public static async Task Test(Type type, string sol1, string sol2)
     {
+        const string InputFieldName = "input";
+
         if (Activator.CreateInstance(type) is BaseDay instance)
         {
             // Gross hack to set the input data for the tests
             type.GetRuntimeFields()
                 .FirstOrDefault(a => string.Equals(a.Name, InputFieldName))?
-                .SetValue(instance, File.ReadAllText(Path.Combine(TestInputsDir, instance.InputFilePath).Replace(@$"\{InputsDir}\", @"\").Replace(@$"/{InputsDir}/", @"/")));
+                .SetValue(instance, File.ReadAllText(ResolveTestInputPath(instance.InputFilePath)));
 
             await Assert.ThatAsync(async () => await instance.Solve_1(), Is.EqualTo(sol1));
             await Assert.ThatAsync(async () => await instance.Solve_2(), Is.EqualTo(sol2));
@@ -29,6 +27,17 @@ public static class Tests
         else
         {
             Assert.Fail($"{type} is not a BaseDay");
+        }
+
+        static string ResolveTestInputPath(string inputFilePath)
+        {
+            const string TestInputsDir = "TestInputs";
+            const string InputsDir = "Inputs";
+
+            // Ensure cross-platform compatibility by resolving the path and removing unnecessary parts
+            return Path.GetFullPath(Path.Combine(TestInputsDir, inputFilePath))
+                .Replace($"{Path.DirectorySeparatorChar}{InputsDir}{Path.DirectorySeparatorChar}", $"{Path.DirectorySeparatorChar}")
+                .Replace($"{Path.AltDirectorySeparatorChar}{InputsDir}{Path.AltDirectorySeparatorChar}", $"{Path.AltDirectorySeparatorChar}");
         }
     }
 }
