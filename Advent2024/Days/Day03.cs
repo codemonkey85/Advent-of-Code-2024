3a28f5758f5fdc98f4911f8491793dcf78906d41
Michael Bond <michael@bondcodes.com>
2024-12-03 19:36:12 -0500

    Complete day 3 part 1

--- Advent2024/Days/Day03.cs
+++ Advent2024/Days/Day03.cs
@@ -1,23 +1,45 @@
 ﻿namespace Advent2024.Days;
 
 // https://adventofcode.com/2024/day/3
-public sealed class Day03 : BaseDay
+public partial class Day03 : BaseDay
 {
     private const bool TestMode = true;
 
     private readonly string Input;
 
-    private const string TestInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    private const string Part1TestInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    
+    private const string Part2TestInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
-    public Day03() => Input = TestMode ? TestInput : File.ReadAllText(InputFilePath);
+    public Day03() => Input = File.ReadAllText(InputFilePath);
 
     public override ValueTask<string> Solve_1()
     {
-        return new(string.Empty);
+        var input = TestMode ? Part1TestInput : Input;
+
+        var nums = Part1MultiplyInstructionsRegex()
+            .Matches(input)
+            .Select(match => match?.ToString()?[4..^1].Split(',', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse) ?? [])
+            .ToList();
+
+        var sum = 0;
+
+        foreach (var n in nums)
+        {
+            sum += n.Aggregate((a, b) => a * b);
+        }
+
+        return new(sum.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
+        var input = TestMode ? Part2TestInput : Input;
+
         return new(string.Empty);
     }
+
+    [GeneratedRegex(@"mul\([0-9]{1,3},[0-9]{1,3}\)")]
+    private static partial Regex Part1MultiplyInstructionsRegex();
 }
--- Advent2024/GlobalUsings.cs
+++ Advent2024/GlobalUsings.cs
@@ -1,1 +1,2 @@
-﻿global using AoCHelper;
+﻿global using System.Text.RegularExpressions;
+global using AoCHelper;
