f4d8d026d48f6a57458febe0b545e7f196d2bcd4
Michael Bond <michael@bondcodes.com>
2024-12-07 16:16:53 -0500

    Stub out day 7

--- Advent2024.Tests/DaysTests.cs
+++ Advent2024.Tests/DaysTests.cs
@@ -20,6 +20,9 @@ public static class Tests
     [TestCase(typeof(Day06), "41", "")]
     public static async Task Day6Test(Type type, string sol1, string sol2) => await TestInternal(type, sol1, sol2);
 
+    [TestCase(typeof(Day07), "", "")]
+    public static async Task Day7Test(Type type, string sol1, string sol2) => await TestInternal(type, sol1, sol2);
+
     private static async Task TestInternal(Type type, string sol1, string sol2)
     {
         if (Activator.CreateInstance(type) is not MyBaseDay instance)
---
+++ Advent2024/Days/Day07.cs
@@ -0,0 +1,29 @@
+﻿namespace Advent2024.Days;
+
+// https://adventofcode.com/2024/day/7
+public sealed class Day07 : MyBaseDay
+{
+    public override string? Part1TestInput { get; set; } = """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+        """;
+
+    public Day07() => Input = File.ReadAllText(InputFilePath);
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new(string.Empty);
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new(string.Empty);
+    }
+}
