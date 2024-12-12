5293cdac8775af0e739531d1c96ea98d5b5cb6b3
Michael Bond <michael@bondcodes.com>
2024-12-12 09:32:54 -0500

    Add in day 11 sample data

--- Advent2024/Days/Day11.cs
+++ Advent2024/Days/Day11.cs
@@ -3,9 +3,7 @@
 // https://adventofcode.com/2024/day/11
 public sealed class Day11 : MyBaseDay
 {
-    public override string? Part1TestInput { get; set; } = """
-
-                                                           """;
+    public override string? Part1TestInput { get; set; } = "0 1 10 99 999";
 
     public Day11() => Input = File.ReadAllText(InputFilePath);
 
