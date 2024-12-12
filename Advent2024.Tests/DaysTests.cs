219e8391fb1e81dec6d4e6991ed0936eaeb7289c
Michael Bond <michael@bondcodes.com>
2024-12-12 09:32:21 -0500

    Stub out day 12

--- Advent2024.Tests/DaysTests.cs
+++ Advent2024.Tests/DaysTests.cs
@@ -35,6 +35,9 @@ public static class Tests
     [TestCase(typeof(Day11), "", "")]
     public static async Task Day11Test(Type type, string sol1, string sol2) => await TestInternal(type, sol1, sol2);
 
+    [TestCase(typeof(Day12), "", "")]
+    public static async Task Day12Test(Type type, string sol1, string sol2) => await TestInternal(type, sol1, sol2);
+
     private static async Task TestInternal(Type type, string sol1, string sol2)
     {
         if (Activator.CreateInstance(type) is not MyBaseDay instance)
